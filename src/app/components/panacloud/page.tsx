4af8ff5c9b46e304c57d5f12baf5cdc9fab8c536
syeda <sbtshakeel61@gmail.com>
2025-09-03 12:16:20 +0500

    add project

---
+++ src/app/components/panacloud/page.tsx
@@ -0,0 +1,41 @@
+import { FcLink } from "react-icons/fc";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Panacloud() {
+  return (
+    <section className="py-16 bg-gray-100 min-h-screen flex flex-col items-center justify-center">
+      <div className="max-w-5xl mx-auto px-4 flex flex-col items-center justify-center text-center gap-11">
+        <Link
+          href="https://huggingface.co/spaces/sabahat12/panacloud-agent"
+          className="text-blue-600 hover:text-blue-800 underline text-2xl font-semibold mb-6 flex gap-1"
+        ><FcLink className="text-blue-600 hover:text-blue-800 underline pt-2"/> Go to Panacloud AI Assistant
+        </Link>
+        <h1 className="text-3xl lg:text-4xl font-bold underline text-black/80 mb-4">
+          Features
+        </h1>
+        <ul className="list-none space-y-2 text-base lg:text-lg text-gray-600">
+          <li>Chainlit</li>
+          <li>OpenAI Agent SDK</li>
+        </ul>
+      </div>
+   
+    <div className="max-w-5xl mx-auto px-3 flex flex-col items-center justify-center text-center mt-10">
+      <h1 className="text-xl font-semibold p-5 text-black/80">Simple WorkFlow Graph</h1>
+      <div className="w-full">
+        <Image
+          src="/panacloudagent.png"
+          alt="Project 4"
+          width={500}
+          height={150}
+          priority
+          className="w-full max-w-[500px] h-auto object-contain rounded-md mb-4 border-4 sm:border-8 border-black"
+        />
+          
+
+      </div>
+    </div>
+          
+    </section>
+  );
+}
--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -14,10 +14,33 @@ export default function ProjectCards() {
         <h1 className={` ${merrweight.className} font-bold text-2xl text-black/80`}>OpenAI Agents Projects</h1>
 </div>
 
-         <div className="flex flex-col md:flex-row gap-6 max-w-5xl mx-auto min-h-screen items-center justify-center p-4">
+         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-5xl mx-auto min-h-screen items-center justify-center p-4">
    
     
-      {/* Card 1 */}
+      {/* Card 4 */}
+
+    <div className="bg-white rounded-lg shadow-lg  shadow-blue-600 overflow-hidden flex flex-col items-center text-center p-6 w-full max-w-md  hover:scale-105  hover:shadow-lg hover:shadow-pink-500 active:shadow-lg active:shadow-pink-500">
+        <Link href="/components/panacloud">
+        <Image
+
+           src="/pana.jpg"
+          alt="Project 4"
+          width={500}
+          height={192}
+          className="w-full h-48 object-cover rounded-md mb-4"
+        />
+        <h1 className="text-2xl md:text-3xl font-bold text-black/80 mb-2">Panacloud AI Assistant</h1>
+        <p className="text-base md:text-lg text-gray-600 mb-4">Advanced AI Automation</p>
+        <p className="text-sm md:text-base text-gray-500 h-28">
+         The Panacloud AI Assistant works as both an Orchestrator and a Triage Agent — orchestrating DevOps and OpenAI agents, while efficiently triaging tasks related to web and mobile app development.
+        </p>
+        </Link>
+      </div>
+
+
+
+
+
       <div className="bg-white rounded-lg shadow-lg  shadow-blue-600 overflow-hidden flex flex-col items-center text-center p-6 w-full max-w-md  hover:scale-105  hover:shadow-lg hover:shadow-pink-500 active:shadow-lg active:shadow-pink-500">
         <Link href="/components/refund">
         <Image
@@ -31,10 +54,13 @@ export default function ProjectCards() {
         <h1 className="text-2xl md:text-3xl font-bold text-black/80 mb-2">Refund AI Assistant</h1>
         <p className="text-base md:text-lg text-gray-600 mb-4">Advanced AI Automation</p>
         <p className="text-sm md:text-base text-gray-500 h-28">
-          A refund AI assistant streamlines return processes with an intuitive frontend interface. It guides users through refund requests, ensuring clarity and efficiency..
+          A refund AI assistant streamlines return processes with an intuitive frontend interface. It guides users through refund requests, ensuring clarity and efficiency.
         </p>
         </Link>
       </div>
+
+
+
       {/* Card 2 */}
       <div className="bg-white rounded-lg shadow-lg shadow-blue-600 overflow-hidden flex flex-col items-center text-center p-6 w-full max-w-md hover:scale-105  hover:shadow-lg hover:shadow-pink-500 active:shadow-lg active:shadow-pink-500" >
        <Link href="/components/weather">
@@ -48,7 +74,7 @@ export default function ProjectCards() {
         <h1 className="text-2xl md:text-3xl font-bold text-black/80 mb-2">Weather AI Assistant</h1>
         <p className="text-base md:text-lg text-gray-600 mb-4">Advanced AI Automation</p>
         <p className="text-sm md:text-base text-gray-500 h-28 " >
-         A weather AI assistant delivers real-time forecasts and personalized weather updates, It analyzes patterns to provide accurate temperature and precipitation data..
+         A weather AI assistant delivers real-time forecasts and personalized weather updates, It analyzes patterns to provide accurate temperature and precipitation data.
         </p>
         </Link>
       </div>
