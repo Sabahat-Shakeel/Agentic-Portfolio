9179a17ded371d906f012a5d814d5b6053565474
syeda <sbtshakeel61@gmail.com>
2025-09-04 15:44:46 +0500

    add code css

--- src/app/components/n8n_io/n8n.tsx
+++ src/app/components/n8n_io/n8n.tsx
@@ -4,18 +4,17 @@ const merrweight = Merriweather({subsets:['latin-ext'] , weight :['900']})
 
 export default function Neightn() {
   return (
-    <main className="h-screen w-full flex flex-col items-center justify-center bg-gray-50">
+    <main className="h-96 gap-11 w-full flex flex-col items-center justify-center p-5 mt-11 ">
     
-      <h1 className={`${merrweight.className} p-6 text-4xl font-bold text-black/80`}>
+      <h1 className={`${merrweight.className} text-4xl font-bold text-black/80`}>
         N8n.io Projects
       </h1>
 
   
-      <div className="w-96 h-80 flex items-center shadow-blue-600 justify-center rounded-2xl shadow-lg bg-white border relative overflow-hidden">
-         <h2 className="text-xl font-semibold text-black/80 text-gray-600 z-10 opacity-80">
+      <div className="w-96 h-80 flex items-center shadow-blue-0 justify-center rounded-2xl shadow-lg drop-shadow-2xl bg-white border relative overflow-hidden portrait:w-80 opacity-75 ">
+         <h2 className="text-xl font-semibold text-black/70 z-10 opacity-80">
           Working on ...
         </h2>
-
 
         <div className="absolute inset-0 bg-gradient-to-r from-transparent via-gray-200 to-transparent animate-[shimmer_2s_infinite]" />
       </div>
@@ -30,7 +29,8 @@ export default function Neightn() {
             transform: translateX(100%);
           }
         }
-      `}</style>
+      `}
+      </style>
     </main>
   );
 }
